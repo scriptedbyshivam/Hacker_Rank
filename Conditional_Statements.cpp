5e8518b606e693f7989bab0bc0db0ce92e243ad0
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-09-18 20:52:28 +0530

    Problem 4

---
+++ Conditional_Statements.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+// string ltrim(const string &);
+// string rtrim(const string &);
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    // string n_temp;
+    // getline(cin, n_temp);
+
+    // int n = stoi(ltrim(rtrim(n_temp)));
+
+    // Write your code here
+    if(n==1){
+        
+        cout<<"one";
+    }
+    else if(n==2){
+        
+        cout<<"two";
+    }
+    else if(n==3){
+        
+        cout<<"three";
+    }
+    else if(n==4){
+        
+        cout<<"four";
+    }
+    else if(n==5){
+        cout<<"five";
+        
+    }
+    else if(n==6){
+           cout<<"six";
+    }
+    else if(n==7){
+        
+        cout<<"seven";
+    }
+    else if(n==8){
+        
+        cout<<"eight";
+    }
+    else if(n==9){
+        
+        cout<<"nine";
+    }
+    else if(n>9)
+    {
+        cout<<"Greater than 9";
+    }
+
+
+    return 0;
+}
+

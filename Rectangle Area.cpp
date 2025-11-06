d7e8fa449a72aaac838f78ddd57ca9bbb10d3379
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-06 20:51:15 +0530

    Rectangle Area

---
+++ Rectangle Area.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+
+class Rectangle
+{
+public:
+    int width, height;
+
+    void display()
+    {
+        cout << width << " " << height << endl;
+    }
+};
+
+class RectangleArea : public Rectangle
+{
+public:
+    void read_input()
+    {
+        cin >> width >> height;
+    }
+    void display()
+    {
+        cout << width * height << endl;
+    }
+};
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+
+    /*
+     * Print the area
+     */
+    r_area.display();
+    // derived class base ke function ko replace (override) kar rahi hai.
+    //  Overriding = Parent-child classes me, same function name & parameters, naya behavior.
+
+    return 0;
+}
